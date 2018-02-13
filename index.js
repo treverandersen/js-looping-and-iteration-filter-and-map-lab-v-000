b6d3e117146eb6da4ccfb1dd9ccb4ce2d9d10264
Trever Andersen <trever.r.andersen@gmail.com>
2018-02-13 20:25:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ function driversWithRevenueOver(drivers, revenue) {
     const result = drivers.filter(driver => driver.revenue > revenue);
     return result;
 }
+
+function driverNamesWithRevenueOver(drivers, revenue) {
+  const result = drivers.filter(driver => driver.revenue > revenue);
+  return result.name;
+}
