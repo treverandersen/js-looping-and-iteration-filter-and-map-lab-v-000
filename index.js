c1523abcc1fa97ca8e25b8f679f4c20d0a20d487
Trever Andersen <trever.r.andersen@gmail.com>
2018-02-13 22:17:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,9 @@ function driversWithRevenueOver(drivers, revenue) {
     return result;
 }
 function driverNamesWithRevenueOver(drivers, revenue) {
-  return driversWithRevenueOver(drivers, revenue).map(driver => driver.name)
+  return driversWithRevenueOver(drivers, revenue).map(driver => driver.name);
+}
+function exactMatch(drivers, obj) {
+  const matched = drivers.filter(driver => driver.name === obj || driver.revenue === obj);
+  return matched;
 }
