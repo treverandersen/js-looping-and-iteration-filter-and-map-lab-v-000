0be21087f57d3a2e021e1bc98408e72400a9acd1
Trever Andersen <trever.r.andersen@gmail.com>
2018-02-13 19:14:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Code your solution here:
 function driversWithRevenueOver(drivers, revenue) {
-    const result = drivers.filter(driver => driver.revenue > revenue);
+    const result = drivers.filter(drivers.revenue > revenue);
     return result;
 }
